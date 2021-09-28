c6e03422581b1e59b45157a2bc5da3886a523dba
Kang Jingliang <45533802+jinglkang@users.noreply.github.com>
2021-09-28 19:52:45 +0800

    Create gene_module_cor.R

---
+++ WGCNA/gene_module_cor.R
@@ -0,0 +1,73 @@
+pH = as.data.frame(datTraits$pH)
+names(pH) = "pH"
+modNames = substring(names(MEs), 3)
+geneModuleMembership = as.data.frame(cor(datExpr, MEs, use = "p"))
+MMPvalue = as.data.frame(corPvalueStudent(as.matrix(geneModuleMembership), nSamples))
+names(geneModuleMembership) = paste("MM", modNames, sep="")
+names(MMPvalue) = paste("p.MM", modNames, sep="")
+geneTraitSignificance = as.data.frame(cor(datExpr, pH, use = "p"))
+GSPvalue = as.data.frame(corPvalueStudent(as.matrix(geneTraitSignificance), nSamples))
+names(geneTraitSignificance) = paste("GS.", names(pH), sep="")
+geneInfo0 = data.frame(moduleColor=dynamicColors, geneTraitSignificance, GSPvalue)
+modOrder = order(-abs(cor(MEs, pH, use = "p")))
+for (mod in 1:ncol(geneModuleMembership))
+{
+  oldNames = names(geneInfo0)
+  geneInfo0 = data.frame(geneInfo0, geneModuleMembership[, modOrder[mod]],
+                         MMPvalue[, modOrder[mod]]);
+  names(geneInfo0) = c(oldNames, paste("MM.", modNames[modOrder[mod]], sep=""),
+                       paste("p.MM.", modNames[modOrder[mod]], sep=""))
+}
+geneOrder = order(geneInfo0$moduleColor, -abs(geneInfo0$GS.pH))
+geneInfo = geneInfo0[geneOrder, ]
+write.csv(geneInfo, file="geneInfo_pH.csv")
+
+
+Salinity = as.data.frame(datTraits$Salinity)
+names(Salinity) = "Salinity"
+modNames = substring(names(MEs), 3)
+geneModuleMembership = as.data.frame(cor(datExpr, MEs, use = "p"))
+MMPvalue = as.data.frame(corPvalueStudent(as.matrix(geneModuleMembership), nSamples))
+names(geneModuleMembership) = paste("MM", modNames, sep="")
+names(MMPvalue) = paste("p.MM", modNames, sep="")
+geneTraitSignificance = as.data.frame(cor(datExpr, Salinity, use = "p"))
+GSPvalue = as.data.frame(corPvalueStudent(as.matrix(geneTraitSignificance), nSamples))
+names(geneTraitSignificance) = paste("GS.", names(Salinity), sep="")
+geneInfo0 = data.frame(moduleColor=dynamicColors, geneTraitSignificance, GSPvalue)
+modOrder = order(-abs(cor(MEs, Salinity, use = "p")))
+for (mod in 1:ncol(geneModuleMembership))
+{
+  oldNames = names(geneInfo0)
+  geneInfo0 = data.frame(geneInfo0, geneModuleMembership[, modOrder[mod]],
+                         MMPvalue[, modOrder[mod]]);
+  names(geneInfo0) = c(oldNames, paste("MM.", modNames[modOrder[mod]], sep=""),
+                       paste("p.MM.", modNames[modOrder[mod]], sep=""))
+}
+geneOrder = order(geneInfo0$moduleColor, -abs(geneInfo0$GS.Salinity))
+geneInfo = geneInfo0[geneOrder, ]
+write.csv(geneInfo, file="geneInfo_Salinity.csv")
+
+
+Length = as.data.frame(datTraits$Length)
+names(Length) = "Length"
+modNames = substring(names(MEs), 3)
+geneModuleMembership = as.data.frame(cor(datExpr, MEs, use = "p"))
+MMPvalue = as.data.frame(corPvalueStudent(as.matrix(geneModuleMembership), nSamples))
+names(geneModuleMembership) = paste("MM", modNames, sep="")
+names(MMPvalue) = paste("p.MM", modNames, sep="")
+geneTraitSignificance = as.data.frame(cor(datExpr, Length, use = "p"))
+GSPvalue = as.data.frame(corPvalueStudent(as.matrix(geneTraitSignificance), nSamples))
+names(geneTraitSignificance) = paste("GS.", names(Length), sep="")
+geneInfo0 = data.frame(moduleColor=dynamicColors, geneTraitSignificance, GSPvalue)
+modOrder = order(-abs(cor(MEs, Length, use = "p")))
+for (mod in 1:ncol(geneModuleMembership))
+{
+  oldNames = names(geneInfo0)
+  geneInfo0 = data.frame(geneInfo0, geneModuleMembership[, modOrder[mod]],
+                         MMPvalue[, modOrder[mod]]);
+  names(geneInfo0) = c(oldNames, paste("MM.", modNames[modOrder[mod]], sep=""),
+                       paste("p.MM.", modNames[modOrder[mod]], sep=""))
+}
+geneOrder = order(geneInfo0$moduleColor, -abs(geneInfo0$GS.Length))
+geneInfo = geneInfo0[geneOrder, ]
+write.csv(geneInfo, file="geneInfo_Length.csv")
